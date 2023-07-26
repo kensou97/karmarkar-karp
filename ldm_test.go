066e9bcb8d77b27d9beb55182e1a6c65e05f0877
Wenkang Zhang <314830391@qq.com>
2023-07-27 02:27:07 +0800

    add tests

--- ldm_test.go
+++ ldm_test.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
+	"sort"
 	"testing"
 )
 
@@ -13,23 +12,58 @@ func (i IntMember) Weight() int {
 }
 
 func TestKarmarkarKarp(t *testing.T) {
-	weights := []int{5, 5, 5, 4, 4, 3, 3, 1}
-	var members []Member
-	for _, weight := range weights {
-		members = append(members, IntMember(weight))
+	cases := []struct {
+		weights  []int
+		k        int
+		expected [][]int
+	}{
+		{
+			weights:  []int{8, 7, 6, 5, 4},
+			k:        2,
+			expected: [][]int{{4, 7, 5}, {8, 6}},
+		},
+		{
+			weights:  []int{8, 7, 6, 5, 4},
+			k:        3,
+			expected: [][]int{{4, 7}, {5, 6}, {8}},
+		},
+		{
+			weights:  []int{5, 5, 5, 4, 4, 3, 3, 1},
+			k:        3,
+			expected: [][]int{{5, 3, 3}, {5, 1, 4}, {4, 5}},
+		},
 	}
-	p := KarmarkarKarp(members, 3)
-	fmt.Println(p)
+	for _, c := range cases {
+		var members []Member
+		for _, weight := range c.weights {
+			members = append(members, IntMember(weight))
+		}
+		p := KarmarkarKarp(members, c.k)
+		if !equals(p.subsets, c.expected) {
+			t.Errorf("subsets %v not equals to %v", p.subsets, c.expected)
+		}
+	}
 }
 
-func TestGenerate(t *testing.T) {
-	var is []int
-	for i := 0; i < 30; i++ {
-		is = append(is, rand.Intn(50))
+func equals(sets []Subset, expected [][]int) bool {
+	if len(sets) != len(expected) {
+		return false
 	}
-	fmt.Println(is)
-	for _, i := range is {
-		fmt.Printf("%d,", i)
+	for i, set := range sets {
+		if len(set.members) != len(expected[i]) {
+			return false
+		}
+		sort.SliceStable(set.members, func(i, j int) bool {
+			return set.members[i].Weight() < set.members[j].Weight()
+		})
+		sort.SliceStable(expected[i], func(p, q int) bool {
+			return expected[i][p] < expected[i][q]
+		})
+		for j, member := range set.members {
+			if member.Weight() != expected[i][j] {
+				return false
+			}
+		}
 	}
-	fmt.Println()
+	return true
 }
